106c80d8616f7276cc38fdcca831002bb1c1f016
Jether Balabagno <76276277+jether360@users.noreply.github.com>
2025-01-22 13:55:50 +0800

    update skills content and design

--- src/app/page.js
+++ src/app/page.js
@@ -14,42 +14,43 @@ export default function Home() {
   }, []);
 
   return (
-    <div  style={{ backgroundColor: "white" }}>
-    <main
-      className={`flex max-h-screen flex-col items-center justify-between p-10 ${
-        rendered ? "opacity-100 scale-100" : "opacity-0 scale-90" // Apply opacity and scale based on the rendered state
-      } transition-all duration-500 ease-in-out`}
-      style={{ backgroundColor: "white" }}
-    >
-      <Image src={Pic} alt="" width={200}/>
-      <h1 className="lg:text-4xl font-bold text-gray-600">
-        Hello, I&lsquo;m Jether B. Balabagno
-      </h1>
-      <p className="lg:text-lg text-gray-700 mt-10">
-        `Enthusiastic and takes pleasure in creating innovative things.`
-      </p>
-      <h2 className="lg:text-2xl font-semibold text-gray-500 mt-10">
-        Software Developer
-      </h2>
+    <div style={{ backgroundColor: "#fff", fontFamily: 'Arial, sans-serif', padding: '20px' }}>
+      <main
+        className={`flex max-h-screen flex-col items-center justify-between p-10 ${
+          rendered ? "opacity-100 scale-100" : "opacity-0 scale-90"
+        } transition-all duration-500 ease-in-out`}
+        style={{ backgroundColor: "white", borderRadius: '10px', boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)', maxWidth: '600px', margin: 'auto' }}
+      >
+        <Image src={Pic} alt="Profile Picture" width={150} height={150} className="rounded-full shadow-md"/>
+        <h1 className="lg:text-4xl font-bold text-gray-600 mt-5">
+          Hello, I&lsquo;m Jether B. Balabagno
+        </h1>
+        <p className="lg:text-lg text-gray-700 mt-5 text-center">
+          Enthusiastic and takes pleasure in creating innovative things.
+        </p>
+        <h2 className="lg:text-2xl font-semibold text-gray-500 mt-5">
+          Software Developer
+        </h2>
 
-      <div className="flex mt-10">
-        <a
-          href="https://www.linkedin.com/in/jether-balabagno-421b42204/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="mr-4"
-        >
-          <FaLinkedin className="text-4xl mb-2 cursor-pointer border-2 border-gray-500 rounded-lg p-2 shadow-lg transition-transform hover:scale-110" style={{color: 'black'}}/>
-        </a>
-        <a
-          href="https://github.com/jether360"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FaGithub className="text-4xl mb-2 cursor-pointer border-2 border-gray-500 rounded-lg p-2 shadow-lg transition-transform hover:scale-110" style={{color: 'black'}}/>
-        </a>
-      </div>
-    </main>
+        <div className="flex mt-10 space-x-4">
+          <a
+            href="https://www.linkedin.com/in/jether-balabagno-421b42204/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-600 hover:text-blue-800"
+          >
+            <FaLinkedin className="text-4xl mb-2 cursor-pointer border-2 border-gray-500 rounded-full p-2 shadow-lg transition-transform hover:scale-110" />
+          </a>
+          <a
+            href="https://github.com/jether360"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-black hover:text-gray-800"
+          >
+            <FaGithub className="text-4xl mb-2 cursor-pointer border-2 border-gray-500 rounded-full p-2 shadow-lg transition-transform hover:scale-110" />
+          </a>
+        </div>
+      </main>
     </div>
   );
 }
--- src/app/skills/page.js
+++ src/app/skills/page.js
@@ -5,57 +5,70 @@ import {
   FaReact,
   FaMobile,
   FaPython,
-  FaFire,
-  FaDatabase,
-  FaServer,
 } from "react-icons/fa";
-import { useEffect, useState } from "react"; // Import useEffect and useState
+import { SiTypescript, SiCypress,SiNextdotjs } from "react-icons/si";
+import { useEffect, useState } from "react";
 
 const SkillsList = () => {
-  const [rendered, setRendered] = useState(false); // State to track if the component has rendered
+  const [rendered, setRendered] = useState(false);
 
-  // useEffect to set rendered to true after the component has mounted
   useEffect(() => {
     setRendered(true);
   }, []);
 
+  const developmentSkills = [
+    { icon: <FaJs className="mr-2 text-blue-500" />, name: "JavaScript", rating: 9 },
+    { icon: <SiTypescript className="mr-2 text-blue-500" />, name: "TypeScript", rating: 8 },
+    { icon: <FaReact className="mr-2 text-blue-500" />, name: "React Js", rating: 9 },
+    { icon: <SiNextdotjs className="mr-2 text-blue-500" />, name: "Next Js", rating: 7 },
+    { icon: <FaMobile className="mr-2 text-blue-500" />, name: "React Native", rating: 9 },
+  ];
+
+  const automationSkills = [
+    { icon: <FaPython className="mr-2 text-blue-500" />, name: "Selenium Python", rating: 6 },
+    { icon: <SiCypress className="mr-2 text-blue-500" />, name: "Cypress", rating: 7 },
+  ];
+
   return (
-    <div style={{ backgroundColor: "white" }}>
+    <div style={{ backgroundColor: "#fff" }}>
       <div
-        className={`flex min-h-screen  flex-col items-center justify-start lg:p-10 ${
-          rendered ? "opacity-100 scale-100" : "opacity-0 scale-90" // Apply opacity and scale based on the rendered state
+        className={`flex min-h-screen flex-col items-center justify-start lg:p-10 ${
+          rendered ? "opacity-100 scale-100" : "opacity-0 scale-90"
         } transition-all duration-500 ease-in-out`}
-        //style={{ backgroundColor: "white" }}
       >
-        <h1 className="text-2xl font-bold mb-4">Skills</h1>
-        <div className="grid grid-cols-2 md:grid-cols-4 gap-10">
-          <div className="flex items-center">
-            <FaJs className="mr-2 text-blue-500" />
-            <span className="text-gray-500">JavaScript</span>
+        <h1 className="text-3xl font-extrabold text-gray-700 mb-6">My Skills</h1>
+
+        <div className="w-full max-w-4xl">
+          <h2 className="text-2xl font-semibold text-blue-600 mb-4 border-b-2 border-blue-400">Development Skills</h2>
+          <div className="grid grid-cols-2 md:grid-cols-3 gap-6 mb-10">
+            {developmentSkills.map((skill, index) => (
+              <div
+                className="flex flex-col items-center bg-white shadow-lg rounded-lg p-4 transition-transform transform hover:scale-105"
+                key={index}
+              >
+                <div className="flex items-center mb-3">
+                  {skill.icon}
+                  <span className="text-gray-800 font-medium text-lg">{skill.name}</span>
+                </div>
+                <div className="text-yellow-500 font-bold">Rating: {skill.rating}/10</div>
+              </div>
+            ))}
           </div>
-          <div className="flex items-center">
-            <FaReact className="mr-2 text-blue-500" />
-            <span className="text-gray-500">React Js</span>
-          </div>
-          <div className="flex items-center">
-            <FaMobile className="mr-2 text-blue-500" />
-            <span className="text-gray-500">React Native</span>
-          </div>
-          <div className="flex items-center">
-            <FaPython className="mr-2 text-blue-500" />
-            <span className="text-gray-500">Selenium Python</span>
-          </div>
-          <div className="flex items-center">
-            <FaFire className="mr-2 text-blue-500" />
-            <span className="text-gray-500">Firebase</span>
-          </div>
-          <div className="flex items-center">
-            <FaDatabase className="mr-2 text-blue-500" />
-            <span className="text-gray-500">Django Rest Framework</span>
-          </div>
-          <div className="flex items-center">
-            <FaServer className="mr-2 text-blue-500" />
-            <span className="text-gray-500">Next Js</span>
+
+          <h2 className="text-2xl font-semibold text-green-600 mb-4 border-b-2 border-green-400">Automation Testing Skills</h2>
+          <div className="grid grid-cols-2 md:grid-cols-3 gap-6">
+            {automationSkills.map((skill, index) => (
+              <div
+                className="flex flex-col items-center bg-white shadow-lg rounded-lg p-4 transition-transform transform hover:scale-105"
+                key={index}
+              >
+                <div className="flex items-center mb-3">
+                  {skill.icon}
+                  <span className="text-gray-800 font-medium text-lg">{skill.name}</span>
+                </div>
+                <div className="text-yellow-500 font-bold">Rating: {skill.rating}/10</div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
